52e5d000dcb66503145dbf38cc85703d1bf2559a
Tyler Dickenson <TJDickenson75@gmail.com>
2026-04-29 02:10:41 +0100

    Adding info to listings migration and modifying user migration to add role/company name

--- app/Models/listing.php
+++ app/Models/listing.php
@@ -4,7 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class listing extends Model
+class Listing extends Model
 {
-    //
+    public function seller()
+    {
+        return $this->belongsTo(User::class, 'seller_id');
+    }
 }
